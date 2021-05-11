bde704aa53469a33802d94b0ba7d55d3fade7e93
Виктория <viktoria.servetnik@yandex.ru>
2021-05-11 15:21:15 +0300

    новый

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $("#btn1").on("click", (event) => {
+//     $("div").animate (
+//         {
+//             "left": "500px"
+//         }, 1500, "linear", () => {
+//             alert ("Ящик закончил двигаться")
+//         })
+// })
+
+
+ $("#btn1").on("click", (event) => {
+    $("p").fadeOut("slow")
+     })
+
+ $("#btn2").on("click", (event) => {
+ $("p").fadeIn("slow")
+ })
+
+
+ 
